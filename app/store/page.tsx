c4ba830820de7e962e14466c24749512a59f7d41
nidalsinelnyk3 <nidalsinelnyk@gmail.com>
2024-07-22 17:32:00 +0300

    checking guest user

---
+++ app/store/page.tsx
@@ -0,0 +1,22 @@
+import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs';
+
+import prismadb from '@/lib/prismadb';
+import { getStoreByGuest } from '@/actions/get-store-by-guest';
+import { signOutGuest } from '@/actions/sign-out-guest';
+
+export default async function DashboardLayout() {
+
+    const guestStore = await getStoreByGuest();
+    const guestId = guestStore?.guestId;
+
+    const { userId } = auth();
+
+    signOutGuest()
+
+    if (!userId && !guestId) redirect('/sign-in');
+
+    redirect('/');
+
+    return null
+};
