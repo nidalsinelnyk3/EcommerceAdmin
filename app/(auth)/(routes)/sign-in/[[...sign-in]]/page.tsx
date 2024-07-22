ac75e0425fba5b2160473cdf3415fbddc41c3a7d
nidalsinelnyk3 <nidalsinelnyk@gmail.com>
2024-07-22 17:32:00 +0300

    removing extra unused variables

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,6 @@
 import SignInGuest from "@/components/sign-in-guest";
 import { SignIn } from "@clerk/nextjs";
 
-
 export default async function Page() {
 
     return (
--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from 'next/navigation';
 import { auth } from '@clerk/nextjs';
 
-import prismadb from '@/lib/prismadb';
 import { getStoreByGuest } from '@/actions/get-store-by-guest';
 import { signOutGuest } from '@/actions/sign-out-guest';
 
